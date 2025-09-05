20956816990e11b47eeed6f6bc7c79f52b1834d7
Jhone-cmd <jhone.1941@gmail.com>
2025-09-05 09:39:54 -0300

    feat: starting apollo server integrated with fastify, and first query

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
+import { ApolloServer } from "@apollo/server"
+import fastifyApollo, {
+  fastifyApolloDrainPlugin,
+} from "@as-integrations/fastify"
 import cors from "@fastify/cors"
 import fastify from "fastify"
 import { connectDB } from "./config/connect-database"
+import { typeDefs } from "./graphql/types/type-defs"
 
 export const app = fastify()
-app.register(cors)
 
-connectDB()
-console.log("hello world")
+async function init() {
+  connectDB()
+
+  const resolvers = {
+    Query: {
+      hello: () => "hello world, graphql",
+    },
+  }
+
+  const apollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [fastifyApolloDrainPlugin(app)],
+  })
+
+  await apollo.start()
+
+  app.register(cors)
+  await app.register(fastifyApollo(apollo))
+}
+
+init()
